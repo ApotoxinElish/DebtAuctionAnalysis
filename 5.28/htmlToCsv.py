b4832211afa237e0ca7266bd900ba1f54ee1826a
ApotoxinElish <19079181d@connect.polyu.hk>
2022-05-29 17:05:39 +0800

    Update

--- 5.28/htmlToCsv.py
+++ 5.28/htmlToCsv.py
@@ -1,3 +1,7 @@
+from matplotlib.pyplot import text
+import pandas as pd
+
+
 def readFileContent(fileName):
     with open(fileName) as file:
         content = file.read()
@@ -33,6 +37,8 @@ DataDict = {
 def parseLine(line):
     offset = line.find('">') + 2
     offsetB = line.find(" &gt;", offset)
+    if line[offset:offsetB] in ["FILE", "RELY", "DENY"]:
+        return
     DataDict["Type"].append(line[offset:offsetB])
 
     offset = line.find("&gt; ", offsetB) + 5
@@ -59,15 +65,38 @@ def parseLine(line):
         offsetB = line.find(" |", offset)
         DataDict["bid"].append(line[offset:offsetB])
 
-    offset = line.find("Took Rate: ", offsetB, offsetB + 10) + 11
+    offset = line.find("Took Rate: ", offsetB) + 11
     if offset == 10:
         DataDict["Took Rate"].append("")
         offset = offsetB
     else:
-        offsetB = line.find(" |", offset)
-        DataDict["Took Rate"].append(line[offset:offsetB])
+        offsetB = line.find("<b ", offset)
+        tookRate = line[offset:offsetB]
+        offset = line.find('">', offsetB) + 2
+        offsetB = line.find(" ", offset)
+        tookRate += line[offset:offsetB]
+        DataDict["Took Rate"].append(tookRate)
 
-    print(DataDict)
+    offset = line.find("| ", offsetB) + 2
+    offsetB = line.find("% |", offset) + 1
+    if offsetB == 0:
+        DataDict["Rate"].append("")
+    else:
+        DataDict["Rate"].append(line[offset:offsetB])
+
+    offset = line.find("Price: ", offsetB) + 7
+    offsetB = line.find(" |", offset)
+    DataDict["Price"].append(line[offset:offsetB])
+
+    offset = line.find("from: ", offsetB) + 6
+    offsetB = line.find(" |", offset)
+    DataDict["from"].append(line[offset:offsetB])
+
+    offset = line.find('href="', offsetB) + 6
+    offsetB = line.find('">', offset)
+    DataDict["Link"].append(line[offset:offsetB])
+
+    # print(DataDict)
 
 
 def main():
@@ -78,9 +107,19 @@ def main():
         line, offset = readEachLine(content, offset)
         if line == None:
             break
-        print(line)
+        # print(line)
         parseLine(line)
-        break
+        # break
+
+    data = pd.DataFrame(DataDict)
+    data["ID"] = data["ID"].astype(int)
+    data["Time"] = pd.to_datetime(data["Time"], format="%d %b %Y, %H:%M:%S")
+    data.sort_values(["ID", "Time", "Type"], inplace=True)
+    data.to_csv("data.csv", index=False)
+
+    # data = pd.read_csv("data.csv")
+
+    # data.to_csv("data.csv", index=False)
 
 
 if __name__ == "__main__":
