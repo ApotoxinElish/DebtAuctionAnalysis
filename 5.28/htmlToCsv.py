7e4943d19d662ba88cce36bd16b931eb2ffc2fb8
ApotoxinElish <19079181d@connect.polyu.hk>
2022-05-29 15:34:21 +0800

    Update htmlToCsv.py

--- 5.28/htmlToCsv.py
+++ 5.28/htmlToCsv.py
@@ -16,8 +16,58 @@ def readEachLine(content, offset):
     return content[eachRowStart:eachRowEnd], eachRowEnd
 
 
+DataDict = {
+    "Type": [],
+    "Time": [],
+    "ID": [],
+    "lot": [],
+    "bid": [],
+    "Took Rate": [],
+    "Rate": [],
+    "Price": [],
+    "from": [],
+    "Link": [],
+}
+
+
 def parseLine(line):
-    pass
+    offset = line.find('">') + 2
+    offsetB = line.find(" &gt;", offset)
+    DataDict["Type"].append(line[offset:offsetB])
+
+    offset = line.find("&gt; ", offsetB) + 5
+    offsetB = line.find(" |", offset)
+    DataDict["Time"].append(line[offset:offsetB])
+
+    offset = line.find("ID: <b>", offsetB) + 7
+    offsetB = line.find("</b>", offset)
+    DataDict["ID"].append(line[offset:offsetB])
+
+    offset = line.find("lot: ", offsetB) + 5
+    if offset == 4:
+        DataDict["lot"].append("")
+        offset = offsetB
+    else:
+        offsetB = line.find(" |", offset)
+        DataDict["lot"].append(line[offset:offsetB])
+
+    offset = line.find("bid: ", offsetB) + 5
+    if offset == 4:
+        DataDict["bid"].append("")
+        offset = offsetB
+    else:
+        offsetB = line.find(" |", offset)
+        DataDict["bid"].append(line[offset:offsetB])
+
+    offset = line.find("Took Rate: ", offsetB, offsetB + 10) + 11
+    if offset == 10:
+        DataDict["Took Rate"].append("")
+        offset = offsetB
+    else:
+        offsetB = line.find(" |", offset)
+        DataDict["Took Rate"].append(line[offset:offsetB])
+
+    print(DataDict)
 
 
 def main():
@@ -28,7 +78,7 @@ def main():
         line, offset = readEachLine(content, offset)
         if line == None:
             break
-        # print(line)
+        print(line)
         parseLine(line)
         break
 
