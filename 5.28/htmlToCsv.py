650fcb0048a3f54a67d8a307f6f33938118afb89
ApotoxinElish <19079181d@connect.polyu.hk>
2022-05-29 12:54:38 +0800

    Update htmlToCsv.py

--- 5.28/htmlToCsv.py
+++ 5.28/htmlToCsv.py
@@ -1,8 +1,34 @@
-def main():
-    with open("5.28/flap.txt") as file:
+def readFileContent(fileName):
+    with open(fileName) as file:
         content = file.read()
-    print(content)
+    return content
+
+
+def readEachLine(content):
+    eachRowStart = content.find("row flip")
+    # print(content[eachRowStart])
+    if eachRowStart == -1:
+        return None
+
+    eachRowEnd = content.find("Info", eachRowStart) + 4
+    # print(content[eachRowEnd])
+
+    return content[eachRowStart:eachRowEnd]
+
+
+def main():
+    content = readFileContent("5.28/flap.txt")
+
+    offset = 0
+    while True:
+        line = readEachLine(content)
+        if line == None:
+            break
+        print(line)
+        # break
 
 
 if __name__ == "__main__":
     main()
+
+# 6309
