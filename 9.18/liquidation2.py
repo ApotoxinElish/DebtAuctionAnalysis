34ffab8c56d12f06bd765b505a3e1ee618fcd597
ApotoxinElish <19079181d@connect.polyu.hk>
2022-09-19 21:23:11 +0800

    Update

---
+++ 9.18/liquidation2.py
@@ -0,0 +1,56 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+tarpath = r"data/maker data dump/maker.liquidation.2.20220403.csv"
+
+
+def read():
+    tarFile = pd.read_csv(tarpath)
+    return tarFile
+
+
+def dataProcess():
+    target = read()
+
+    takeBlockTime = pd.to_datetime(target.take_block_time, format="%Y/%m/%d %H:%M:%S")
+    auctionStartBlockTime = pd.to_datetime(
+        target.auction_start_block_time, format="%Y/%m/%d %H:%M:%S"
+    )
+
+    differ = takeBlockTime - auctionStartBlockTime
+    differ /= np.timedelta64(1, "m")
+    differ = differ.rename("differ")
+
+    startTime = auctionStartBlockTime  # .dt.strftime("%y-%m")
+    startTime = startTime.rename("start_time")
+
+    differDF = pd.concat([startTime, differ], axis=1)
+
+    differDF = (
+        differDF.resample("W-Mon", on="start_time")
+        .mean()
+        .reset_index()
+        .sort_values(by="start_time")
+    )
+    differDF.set_index("start_time", inplace=True)
+
+    # differDF = differDF.groupby("start_time").mean()
+
+    return differDF.sort_index()
+
+
+def plotDraw():
+    result = dataProcess()
+    print(result)
+
+    fig, ax = plt.subplots()
+    ax.bar(result.index, result.differ, 5)
+    # ax.plot(result.index, result.differ)
+    ax.set_title("liquidation2 time differ")
+    plt.show()
+
+
+if __name__ == "__main__":
+    plotDraw()
